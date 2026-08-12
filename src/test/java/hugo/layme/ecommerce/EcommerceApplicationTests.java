b8375e9d1b8518a0c339670b6598bc186bbdd897
Hugo Aragao <hugolayme153@gmail.com>
2026-08-12 02:58:05 -0300

    initial commit

---
+++ src/test/java/hugo/layme/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package hugo.layme.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
