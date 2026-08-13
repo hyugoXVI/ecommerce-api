e0b0b648438bf8199f4350a5379dfae5f6736d48
Hugo Aragao <hugolayme153@gmail.com>
2026-08-13 02:52:21 -0300

    feat: add user details and user details service

---
+++ src/main/java/hugo/layme/ecommerce/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package hugo.layme.ecommerce.security;
+
+import hugo.layme.ecommerce.entity.User;
+import hugo.layme.ecommerce.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Couldn't find user with provided e-mail."));
+
+        return new UserDetailsImpl(user);
+    }
+}
