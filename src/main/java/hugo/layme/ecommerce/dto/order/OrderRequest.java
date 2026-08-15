2ada450a904610f910006b84b9319cf0946cc15d
Hugo Aragao <hugolayme153@gmail.com>
2026-08-15 05:01:51 -0300

    feat: add validation annotations to order request and items list on response

--- src/main/java/hugo/layme/ecommerce/dto/order/OrderRequest.java
+++ src/main/java/hugo/layme/ecommerce/dto/order/OrderRequest.java
@@ -1,9 +1,10 @@
 package hugo.layme.ecommerce.dto.order;
 
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record OrderRequest(@NotNull List<OrderItemRequest> items) {
+public record OrderRequest(@NotNull @NotEmpty List<OrderItemRequest> items) {
 }
--- src/main/java/hugo/layme/ecommerce/dto/order/OrderResponse.java
+++ src/main/java/hugo/layme/ecommerce/dto/order/OrderResponse.java
@@ -4,11 +4,13 @@ import hugo.layme.ecommerce.entity.OrderStatus;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
 
 public record OrderResponse (Long id,
                             Long userId,
                             String userName,
                             BigDecimal total,
                             OrderStatus status,
-                            Instant createdAt){
+                            Instant createdAt,
+                             List<OrderItemResponse> items){
 }
