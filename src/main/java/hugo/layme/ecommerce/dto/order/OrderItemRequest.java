53daa95142bcf1c26f5c60ef80733cd419ec73ce
Hugo Aragao <hugolayme153@gmail.com>
2026-08-17 23:44:30 -0300

    refactor: remove NotNull from order item quantity

--- src/main/java/hugo/layme/ecommerce/dto/order/OrderItemRequest.java
+++ src/main/java/hugo/layme/ecommerce/dto/order/OrderItemRequest.java
@@ -1,13 +1,9 @@
 package hugo.layme.ecommerce.dto.order;
 
-import hugo.layme.ecommerce.entity.Order;
-import hugo.layme.ecommerce.entity.Product;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.PositiveOrZero;
 
-import java.math.BigDecimal;
 
 public record OrderItemRequest(@NotNull Long productId,
-                               @NotNull @Positive int quantity) {
+                               @Positive int quantity) {
 }
