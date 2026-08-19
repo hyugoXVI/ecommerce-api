778591d5809d3d9df02e689301f4fe7b1fb24e5b
Hugo Aragao <hugolayme153@gmail.com>
2026-08-19 03:51:08 -0300

    feat: add Valid annotation to order items

--- src/main/java/hugo/layme/ecommerce/dto/order/OrderRequest.java
+++ src/main/java/hugo/layme/ecommerce/dto/order/OrderRequest.java
@@ -1,10 +1,10 @@
 package hugo.layme.ecommerce.dto.order;
 
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record OrderRequest(@NotEmpty List<OrderItemRequest> items) {
+public record OrderRequest(@NotEmpty @Valid List<OrderItemRequest> items) {
 }
