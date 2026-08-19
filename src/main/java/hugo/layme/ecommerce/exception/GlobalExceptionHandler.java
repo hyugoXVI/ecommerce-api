c2f9d6b23b350b4785393d0385bcd663e63d70e0
Hugo Aragao <hugolayme153@gmail.com>
2026-08-19 00:26:53 -0300

    feat: add validation and opmistic looking handling

--- src/main/java/hugo/layme/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/hugo/layme/ecommerce/exception/GlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package hugo.layme.ecommerce.exception;
 
 import hugo.layme.ecommerce.dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -44,5 +46,43 @@ public class GlobalExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex,
+                                                          HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ":"+ error.getDefaultMessage())
+                .findFirst()
+                .orElse("Invalid request.");
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                message,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(errorResponse);
+    }
+
+    @ExceptionHandler(OptimisticLockingFailureException.class)
+    public ResponseEntity<ErrorResponse> handleOptimisticLookingFailure(OptimisticLockingFailureException ex,
+                                                                        HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.CONFLICT;
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                "Product stock changed during the operation. Try again.",
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(errorResponse);
     }
 }
